58e92374c4067fcb371d6b6f2d74ec41af90092e
Alina <alina90565@gmail.com>
2019-11-16 14:48:24 +0200

    Updates

---
+++ precache-manifest.3178f4cd51462f08725ea623391c62f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdcfe001971a88d7e73f0aafad393023",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "85a35e62de317f5fd06f",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.c3297a66.chunk.css"
+  },
+  {
+    "revision": "506491fb918c888660f8",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.4e0f0993.chunk.js"
+  },
+  {
+    "revision": "85a35e62de317f5fd06f",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.526db000.chunk.js"
+  },
+  {
+    "revision": "1108e16176cf4395a601",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.2073124d.js"
+  }
+]);
